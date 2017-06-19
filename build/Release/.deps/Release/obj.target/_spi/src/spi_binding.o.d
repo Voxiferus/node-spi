1367d2983f41402bf2e33f247484c7cc65597544
Leonid Polyakov <leonid@tibbo.com>
2017-06-19 11:14:33 -0400

    build

---
+++ build/Release/.deps/Release/obj.target/_spi/src/spi_binding.o.d
@@ -0,0 +1,21 @@
+cmd_Release/obj.target/_spi/src/spi_binding.o := mipsel-openwrt-linux-uclibc-g++ -I /sdk/OpenWrt-SDK-ramips-mt7688_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64/staging_dir/target-mipsel_24kec+dsp_uClibc-0.9.33.2/usr/include -L /sdk/OpenWrt-SDK-ramips-mt7688_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/lib -L /sdk/OpenWrt-SDK-ramips-mt7688_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64/staging_dir/target-mipsel_24kec+dsp_uClibc-0.9.33.2/usr/lib '-DNODE_GYP_MODULE_NAME=_spi' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/root/.node-gyp/0.12.7/include/node -I/root/.node-gyp/0.12.7/src -I/root/.node-gyp/0.12.7/deps/uv/include -I/root/.node-gyp/0.12.7/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/_spi/src/spi_binding.o.d.raw   -c -o Release/obj.target/_spi/src/spi_binding.o ../src/spi_binding.cc
+Release/obj.target/_spi/src/spi_binding.o: ../src/spi_binding.cc \
+ ../src/spi_binding.h /root/.node-gyp/0.12.7/deps/v8/include/v8.h \
+ /root/.node-gyp/0.12.7/deps/v8/include/v8stdint.h \
+ /root/.node-gyp/0.12.7/deps/v8/include/v8config.h \
+ /root/.node-gyp/0.12.7/src/node.h \
+ /root/.node-gyp/0.12.7/src/node_version.h \
+ /root/.node-gyp/0.12.7/src/node_object_wrap.h \
+ /root/.node-gyp/0.12.7/src/node_buffer.h \
+ /root/.node-gyp/0.12.7/src/node.h /root/.node-gyp/0.12.7/src/smalloc.h
+../src/spi_binding.cc:
+../src/spi_binding.h:
+/root/.node-gyp/0.12.7/deps/v8/include/v8.h:
+/root/.node-gyp/0.12.7/deps/v8/include/v8stdint.h:
+/root/.node-gyp/0.12.7/deps/v8/include/v8config.h:
+/root/.node-gyp/0.12.7/src/node.h:
+/root/.node-gyp/0.12.7/src/node_version.h:
+/root/.node-gyp/0.12.7/src/node_object_wrap.h:
+/root/.node-gyp/0.12.7/src/node_buffer.h:
+/root/.node-gyp/0.12.7/src/node.h:
+/root/.node-gyp/0.12.7/src/smalloc.h:
